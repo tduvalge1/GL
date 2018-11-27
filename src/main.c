154e1907d520619ea7b478cb9c616edae6fae1d6
tduvalge1 <44801631+tduvalge1@users.noreply.github.com>
2018-11-27 23:10:04 +0100

    voici mon ex58

--- src/main.c
+++ src/main.c
@@ -2,8 +2,48 @@
 //
 
 #include "my_header.h"
+#include "TP_lib_suite.h"
 
+short tirerNumero(void);
 int main(void)
 {
-    return 0;
+
+unsigned char i,j,k,l;
+unsigned char resultat=0,score_max=0;
+unsigned char joueur[6]=						  
+	{4,39,42,16,34,16};
+short tirage[6];
+	
+for(k=0;k<100;k++)	  // Les 100 tirages
+	{
+		resultat=0;
+		initialiserTirage(); 
+		
+		for(l=0;l<6;l++)  // Tirage de la grille du lotot   
+		{
+			tirage[l]=tirerNumero();
+		}
+	
+		
+	for(i=0;i<6;i++)	// Comparaison grille jouer et grille du loto
+		{
+			for(j=0;j<6;j++)
+			{
+				if(joueur[i]== tirage[j])
+				{
+				resultat=resultat+1; // Nombre de chiffre ou nombre identique a la grille du loto
+				}
+			}
+		
+		}
+		if(resultat>score_max)  // Combien de chiffre au max a ete identique
+			{
+				score_max=resultat;
+			}
+		
+	}
+
+
+return 0;
+
 }
